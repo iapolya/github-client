1ff96f36c61ff747477bf00442625074b6a1173f
iapolly <polina7alikina@gmail.com>
2019-12-12 14:25:09 +0300

    + logout action

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import '../App.css';
 import 'antd/dist/antd.css';
+import {Button, PageHeader} from "antd";
+import github from "./static/images/github.png";
+import {Redirect, useHistory} from "react-router-dom";
 
 function App({ children }) {
+    let history = useHistory();
   return (
-    <div style={Layout}>
-        {children}
+    <div>
+        <PageHeader
+            style={{
+                border: '1px solid rgb(235, 237, 240)',
+            }}
+            title={
+                <img height="40px" src={github} alt="logo"/>
+            }
+            subTitle="simple client"
+            extra={[
+                <Button key="3" shape="circle" icon="search" />,
+                <Button key="2" shape="round" icon="user">My profile</Button>,
+                <Button onClick={logOut} key="1" shape="round" type="primary" icon="logout">
+                    Logout
+                </Button>,
+            ]}
+        />
+        <div style={Layout}>
+            {children}
+        </div>
     </div>
   );
+
+    function logOut() {
+        localStorage.removeItem('token');
+        history.push('/login');
+    }
 }
 
 export default App;

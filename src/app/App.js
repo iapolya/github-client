843c9dce04e1d729336a896b1ea0c866f7bd4f38
iapolly <polina7alikina@gmail.com>
2019-12-12 14:28:54 +0300

    - header for login page

--- src/app/App.js
+++ src/app/App.js
@@ -3,28 +3,30 @@ import '../App.css';
 import 'antd/dist/antd.css';
 import {Button, PageHeader} from "antd";
 import github from "./static/images/github.png";
-import {Redirect, useHistory} from "react-router-dom";
+import {useHistory} from "react-router-dom";
 
 function App({ children }) {
     let history = useHistory();
   return (
     <div>
-        <PageHeader
-            style={{
-                border: '1px solid rgb(235, 237, 240)',
-            }}
-            title={
-                <img height="40px" src={github} alt="logo"/>
-            }
-            subTitle="simple client"
-            extra={[
-                <Button key="3" shape="circle" icon="search" />,
-                <Button key="2" shape="round" icon="user">My profile</Button>,
-                <Button onClick={logOut} key="1" shape="round" type="primary" icon="logout">
-                    Logout
-                </Button>,
-            ]}
-        />
+        {history.location.pathname !== '/login' &&
+            <PageHeader
+                style={{
+                    border: '1px solid rgb(235, 237, 240)',
+                }}
+                title={
+                    <img height="40px" src={github} alt="logo"/>
+                }
+                subTitle="simple client"
+                extra={[
+                    <Button key="3" shape="circle" icon="search" />,
+                    <Button key="2" shape="round" icon="user">My profile</Button>,
+                    <Button onClick={logOut} key="1" shape="round" type="primary" icon="logout">
+                        Logout
+                    </Button>,
+                ]}
+            />
+        }
         <div style={Layout}>
             {children}
         </div>
